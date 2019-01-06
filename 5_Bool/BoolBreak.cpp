64c858e86b877b48eb8a288587a61406a4bd3d15
LeeHeungrok <rhh0208@naver.com>
2019-01-06 17:02:43 +0900

    Bool 망가트리기

---
+++ 5_Bool/BoolBreak.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    bool bData;
+
+    bData = 1;
+    cout << "bData : " << ~bData << endl;
+
+    bData = 0;
+    cout << "bData : " << ~bData << endl;
+
+    return 0;
+}
