d53f0473e5122908348ef866883ce0e4eb3e3342
LeeHeungrok <rhh0208@naver.com>
2019-01-17 02:10:57 +0900

    숨겨진 범위를 어떻게 관리하는지 확인

--- 2_Variable/RangeHide.cpp
+++ 2_Variable/RangeHide.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+
+int main(void){
+    int a = 5; // a#1
+    {
+        a = 3; // a#1에 할당
+        int a; // a#2
+        a = 8; // a#2에 할당, a#1은 숨겨짐
+        {
+            a = 7; // a#2에 할당
+        }
+    } // a#2 범위 끝
+    a = 11; // a#1에 할당
+
+    return 0;
+}
