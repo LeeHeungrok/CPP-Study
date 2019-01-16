f55712dbe511a91434c8475d466759f50bc702f8
LeeHeungrok <rhh0208@naver.com>
2019-01-16 23:05:30 +0900

    블로그에서 공부한건 Blog 폴더로 이동. 모던 C++ 입문 책 공부 시작, 첫 프로그램 코딩 완료

---
+++ 1_FirstProgram/FirstProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void){
+    std::cout << "The answer to the Ultimate Question of Lift, \n"
+    << "the Universe, and Everything is: "
+    << std::endl << 6 * 7 << std::endl;
+
+    return 0;
+}
