bd5ddce716b6e411540f782dd4d02e83e5797207
LeeHeungrok <rhh0208@naver.com>
2019-01-17 00:28:13 +0900

    변수 auto 추가

--- 2_Variable/Variable.cpp
+++ 2_Variable/Variable.cpp
@@ -9,9 +9,11 @@ int main(void){
     char c1 = 'a', c2 = 35;
     bool cmp = i1 < pi, // -> true
         happy = true;
+    auto i4 = i3 + 7;
 
     std::cout << i1 << std::endl << i2 << std::endl << i3 << std::endl << pi << std::endl << x << std::endl
-     << y << std::endl << c1 << std::endl << c2 << std::endl << cmp << std::endl << happy << std::endl;
+     << y << std::endl << c1 << std::endl << c2 << std::endl << cmp << std::endl << happy << std::endl
+     << i4 << std::endl;
 
     return 0;
 }
