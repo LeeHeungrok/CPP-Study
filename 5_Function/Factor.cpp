9350a9e396d26a7994077c739bc566c4329e1f03
LeeHeungrok <rhh0208@naver.com>
2019-01-17 23:20:32 +0900

    레퍼런스에 의한 호출, 아무것도 반환하지 않기

--- 5_Function/Factor.cpp
+++ 5_Function/Factor.cpp
@@ -7,8 +7,15 @@ void increment(int x){
     x++;
 }
 
+// 레퍼런스에 의한 호출, 인수를 레퍼런스로 전달(Pass-by-Reference)
+void rIncrement(int& x){
+    x++;
+}
+
 int main(void){
     int i = 4;
     increment(i);   // i가 증가되지 않음
+    cout << "i is " << i << endl;
+    rIncrement(i);   // i가 증가됨
     cout << "i is " << i << endl;
 }
---
+++ 5_Function/Return.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void print_x(int x){
+    cout << "The value x is " << x << endl;
+}
+
+int main(void){
+    
+    print_x(5);
+
+    return 0;
+}
